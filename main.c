6e8cd33d75302570aee109339a135c7ac398761a
poornabuddhima <poornabuddhima8@gmail.com>
2026-09-15 10:02:44 +0530

    display main menu

--- main.c
+++ main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
+void displayMainMenu() ;
 
 
+int main() {
+    int mainChoice = 0;
+
+    while (1) {
+        displayMainMenu();
+        scanf("%d", &mainChoice);
+
+        switch (mainChoice) {
+            case 1: printf("Patient Admit Menu Selected\n");
+            break;
+            case 2: printf("Doctor Channeling Selected\n");
+            break;
+            case 3: printf("Available Beds Selected\n");
+            break;
+            case 4: printf("Available Doctors Selected\n");
+            break;
+            default: printf("Invalid choice!\n");
+            break;
+        }
+    }
     return 0;
+}
+void displayMainMenu() {
+    printf(" Yasaro Hospital\n");
+    printf("1. Patient Admit\n");
+    printf("2. Doctor Channeling\n");
+    printf("3. Available Beds\n");
+    printf("4. Available Doctors\n");
+    printf("Enter your choice: ");
 }
